0eb3479b928ffc2349f21c55c03888a434a71e2d
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-09-27 17:26:54 +0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by A'zamjon Abdumuxtorov on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
